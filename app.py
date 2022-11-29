44efc530bb6fe6b898caf8e064ba52b71ce67567
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-29 20:53:20 -0300

    Create student list

--- app.py
+++ app.py
@@ -4,3 +4,10 @@
 aluno1 = (20221, 'Maria Joaquina', 'mariajoaquina@gmail.com')
 aluno2 = (20222, 'João Gabriel', 'joaogabriel@gmail.com')
 aluno3 = (20223, 'Miguel Oliveira', 'migueloliveira@gmail.com')
+
+# Crie uma lista para representar os alunos matriculados na disciplina
+
+lista_alunos = list()
+lista_alunos.append(aluno1)
+lista_alunos.append(aluno2)
+lista_alunos.append(aluno3)
