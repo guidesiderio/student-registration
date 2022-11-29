a07e6430efac840553524cfa669daffe4e7e8d3c
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-29 20:55:00 -0300

    Create note list

--- app.py
+++ app.py
@@ -11,3 +11,9 @@ lista_alunos = list()
 lista_alunos.append(aluno1)
 lista_alunos.append(aluno2)
 lista_alunos.append(aluno3)
+
+# Crie uma lista de 4 notas para cada aluno
+
+notas_aluno1 = [6, 9, 8, 7]
+notas_aluno2 = [8 , 7.9, 4, 3]
+notas_aluno3 = [10 , 10, 5, 7.7]
