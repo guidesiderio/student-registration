73dd4505aab942658ab7738a6b6918c3c915eaae
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-11-29 20:51:10 -0300

    Create app.py and define tuples

---
+++ app.py
@@ -0,0 +1,6 @@
+# Defina tuplas para representar os dados de 3 alunos
+# Matrícula, nome e email
+
+aluno1 = (20221, 'Maria Joaquina', 'mariajoaquina@gmail.com')
+aluno2 = (20222, 'João Gabriel', 'joaogabriel@gmail.com')
+aluno3 = (20223, 'Miguel Oliveira', 'migueloliveira@gmail.com')
